ef78d42eca28bce698c8b78befb38b50c4ffe76a
maxim <trifonov.max@mail.ru>
2024-07-12 15:39:04 +0300

    feature(router): added root component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,1 +1,12 @@
-export const App = () => <span></span>;
+import { Outlet } from 'react-router-dom';
+
+import { Header } from './components/header';
+
+export const Root = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+};
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,13 +1,25 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { App } from '../app';
+
+import { Root } from '../app';
+import { AntDesPage } from '../app/pages/antD-page';
+import { MuiPage } from '../app/pages/mui-page';
+import { TailWindPage } from '../app/pages/tailwind-page';
 
 export const ROUTER_PATH = {
   app: '/',
+  tailwind: 'tailwind',
+  mui: 'mui',
+  antd: 'antd',
 };
 
 export const router = createBrowserRouter([
   {
     path: ROUTER_PATH.app,
-    element: <App />,
+    element: <Root />,
+    children: [
+      { path: ROUTER_PATH.tailwind, element: <TailWindPage /> },
+      { path: ROUTER_PATH.mui, element: <MuiPage /> },
+      { path: ROUTER_PATH.antd, element: <AntDesPage /> },
+    ],
   },
 ]);
