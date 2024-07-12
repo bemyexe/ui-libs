4e9c36ba153e7ca49f12886380ff81b8463fd04d
maxim <trifonov.max@mail.ru>
2024-07-12 16:02:33 +0300

    refactor(tw-page): added styles

--- src/app/pages/tailwind-page/index.tsx
+++ src/app/pages/tailwind-page/index.tsx
@@ -2,8 +2,8 @@ import { Page } from '../../components/page';
 
 export const TailWindPage = () => {
   return (
-    <Page withHeader className="flex justify-center items-center">
-      tailwind
+    <Page withHeader className="flex justify-center bg-sky-200">
+      <div className="container m-4 bg-sky-500 rounded-xl p-3">test</div>
     </Page>
   );
 };
