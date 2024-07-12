cdf40787beb8413e4724fbbc00e90849126bfb39
maxim <trifonov.max@mail.ru>
2024-07-12 16:02:15 +0300

    refactor(header): added styles

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,3 +1,3 @@
 export const Header = () => {
-  return <header className="min-h-20 border border-blue-800">HEADER</header>;
+  return <header className="min-h-20 bg-blue-900 rounded-b-lg">HEADER</header>;
 };
