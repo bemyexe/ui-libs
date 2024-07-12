c6fc723129dd4e960cf577ab82c05c87e51a72ec
maxim <trifonov.max@mail.ru>
2024-07-12 15:38:32 +0300

    feature(page): added component page

--- src/app/components/page/index.tsx
+++ src/app/components/page/index.tsx
@@ -5,10 +5,15 @@ import { cn } from '../../../helpers/classNames';
 interface PageProps {
   children: ReactNode;
   className?: string;
+  withHeader?: boolean;
 }
 
-export const Page = ({ children, className }: PageProps) => {
+export const Page = ({ children, className, withHeader }: PageProps) => {
   return (
-    <section className={cn('min-h-screen', className)}>{children}</section>
+    <section
+      className={cn(withHeader ? 'h-[calc(100vh-80px)]' : 'h-lvh', className)}
+    >
+      {children}
+    </section>
   );
 };
