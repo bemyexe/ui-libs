0fea909c266954f90edf58c29bac3f60d66e559e
maxim <trifonov.max@mail.ru>
2024-07-13 14:35:58 +0300

    feature(header): added header, header consts, classes

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,3 +1,23 @@
+import { Link } from 'react-router-dom';
+
+import { ROUTER_PATH } from '../../../router/router';
+
 export const Header = () => {
-  return <header className="min-h-20 bg-blue-900 rounded-b-lg">HEADER</header>;
+  return (
+    <header className="header flex justify-center items-end  text-white font-semibold">
+      <nav>
+        <ul className="flex gap-3">
+          <Link to={ROUTER_PATH.tailwind}>
+            <li className="nav">TAILWIND </li>
+          </Link>
+          <Link to={ROUTER_PATH.mui}>
+            <li className="nav">MAERIAL UI</li>
+          </Link>
+          <Link to={ROUTER_PATH.antd}>
+            <li className="nav">ANT DESIGN </li>
+          </Link>
+        </ul>
+      </nav>
+    </header>
+  );
 };
